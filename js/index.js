1947d1acca8e8dc955af9eed9c262e190674de75
Aleksandr <syaskiy9523@gmail.com>
2018-04-07 12:26:52 +0000

    language

---
+++ js/index.js
@@ -0,0 +1,23 @@
+document.getElementById("xhr-get-content").addEventListener("click",e =>{
+   const respons = new XMLHttpRequest();
+  respons.onreadystatechange = function(){
+   if(respons.readyState===4 &&respons.status ===200)
+   {
+      document.getElementById("html-content").innerHTML=respons.responseText;
+   }
+  };
+  respons.open('get','index-for-Ukraine.html?r='+Math.random());
+  respons.send();
+});
+
+document.getElementById("xhr-get-content2").addEventListener("click",e =>{
+   const respons = new XMLHttpRequest();
+  respons.onreadystatechange = function(){
+   if(respons.readyState===4 &&respons.status ===200)
+   {
+      document.getElementById("html-content").innerHTML=respons.responseText;
+   }
+  };
+  respons.open('get','index-for-English.html?r='+Math.random());
+  respons.send();
+});
